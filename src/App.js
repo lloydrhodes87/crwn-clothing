af89961b8b2845e5d250124033cc27c7f6c3f905
Ingrid Alcalde <ingridalcalde@ingrids-mbp-2.home>
2019-11-27 06:51:23 +0000

    use withrouter hoc to implement navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+)
 
 function App() {
   return (
     <div>
-      <Homepage />
+        <Switch>
+            <Route component={Homepage} path="/" exact/>
+            <Route component={HatsPage} path="/hats" />
+        </Switch>
     </div>
   );
 }
