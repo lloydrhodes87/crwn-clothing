9d8cb5d2d8d6f8fb52ff5596e6e89d7761e6b9ac
Ingrid Alcalde <ingridalcalde@ingrids-mbp-2.home>
2019-11-28 20:58:18 +0000

    add sign in page and components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
 import Header from './components/header/header.component';
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { Route, Switch } from 'react-router-dom';
 
 
@@ -13,6 +14,7 @@ function App() {
         <Switch>
             <Route component={Homepage} path="/" exact/>
             <Route component={ShopPage} path="/shop" />
+            <Route component={SignInAndSignUp} path="/signin" />
         </Switch>
     </div>
   );
